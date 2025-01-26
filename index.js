220599ea87c0dac099959b7dec903b11aabcfbd8
JoPed <jonasbjoern@gmail.com>
2025-01-26 18:52:38 +0100

    Fixed errors during build/start due to using variables incorrectly.
    Added dynamic webpack settings for EsLint and StyleLint that ensure only warnings when building for production - no crashes.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 const { join } = require('path');
+const { config } = require('process');
 
 module.exports = {
 	defaultValues: {
@@ -15,40 +16,45 @@ module.exports = {
 		example: {},
 		// We need the --webpack-copy-php to copy the render.php file to the dist folder.
 		// Therefor we override the existing scripts created by @wordpress/create-block
-		// $(npm run paths:js -s)/$(npm run paths:scss -s) is used to dynamically insert the file paths in where they should be used, 
+		// $(npm run paths:js -s)/$(npm run paths:scss -s) is used to dynamically insert the file paths in where they should be used,
 		// without having to use them in each seperate script
 		// $(), command substitution, allows dynamic injecting into scripts
 		// -s it to prevent output to the console when running npm run
 		customScripts: {
-			"build": 'npm run \"format\" && wp-scripts build --webpack-copy-php',
-			"start": 'wp-scripts start --webpack-copy-php',
-			"paths:scss": '\"src/block/editor/scss/*.scss\" \"src/block/frontend/scss/*.scss\"',
-			"paths:js": '\"src/block/editor/js/*.{js,ts,jsx,tsx}\" \"./src/block/editor/js/controls/*.{js,ts,jsx,tsx}\" \"src/block/frontend/js/*.{js,ts,jsx,tsx}\"',
-			"format": "npm run \"format:css:fix\" && npm run \"format:js:fix\"",
-			"format:css": "npx prettier --check --no-error-on-unmatched-pattern $npm_package_scripts_paths_scss",
-			"format:css:fix": "npx prettier --write --no-error-on-unmatched-pattern $npm_package_scripts_paths_scss",
-			"format:fix": "npm run \"format:css:fix\" && npm run \"format:js:fix\"",
-			"format:js": "npx prettier --check --no-error-on-unmatched-pattern $npm_package_scripts_paths_js",
-			"format:js:fix": "npx prettier --write --no-error-on-unmatched-pattern $npm_package_scripts_paths_js",
-			"lint": "npm run \"lint:css\" && npm run \"lint:js\"",
-			"lint:css": "npx stylelint \"src/block/editor/scss/*.scss\" \"src/block/frontend/scss/*.scss\"",
-			"lint:css:fix": "npx stylelint --fix $npm_package_scripts_paths_scss",
-			"lint:fix": "npm run \"lint:css:fix\" && npm run \"lint:js:fix\"",
-			"lint:js": "npx eslint --no-error-on-unmatched-pattern \"*.{js,ts,jsx,tsx}\" $npm_package_scripts_paths_js",
-			"lint:js:fix": "npx eslint --fix --no-error-on-unmatched-pattern \"*.{js,ts,jsx,tsx}\" $npm_package_scripts_paths_js",
+			build: 'cross-env npm run "format" && wp-scripts build --webpack-copy-php --mode=production',
+			start: 'cross-env wp-scripts start --webpack-copy-php --mode=production',
+			format: 'cross-env npm run "format:css:fix" && npm run "format:js:fix"',
+			'format:css':
+				'cross-env npx prettier --check --no-error-on-unmatched-pattern %npm_package_config_scss%',
+			'format:css:fix':
+				'cross-env npx prettier --write --no-error-on-unmatched-pattern %npm_package_config_scss%',
+			'format:fix': 'cross-env npm run "format:css:fix" && npm run "format:js:fix"',
+			'format:js':
+				'cross-env npx prettier --check --no-error-on-unmatched-pattern %npm_package_config_js%',
+			'format:js:fix':
+				'cross-env npx prettier --write --no-error-on-unmatched-pattern %npm_package_config_js%',
+			lint: 'cross-env npm run "lint:css" && npm run "lint:js"',
+			'lint:css': 'cross-env npx stylelint %npm_package_config_scss%',
+			'lint:css:fix': 'cross-env npx stylelint --fix %npm_package_config_scss%',
+			'lint:fix': 'cross-env npm run "lint:css:fix" && npm run "lint:js:fix"',
+			'lint:js':
+				'cross-env npx eslint --no-error-on-unmatched-pattern "*.{js,ts,jsx,tsx}" %npm_package_config_js%',
+			'lint:js:fix':
+				'cross-env npx eslint --fix --no-error-on-unmatched-pattern "*.{js,ts,jsx,tsx}" %npm_package_config_js%',
 		},
 		// Default value if build/index.js, we use dist/main.js
 		customPackageJSON: {
 			main: 'dist/main.js',
+			config: {
+				scss: 'echo "src/block/editor/scss/*.scss" "src/block/frontend/scss/*.scss"',
+				js: 'echo "src/block/editor/js/*.{js,ts,jsx,tsx}" "src/block/editor/js/controls/*.{js,ts,jsx,tsx}" "src/block/frontend/js/*.{js,ts,jsx,tsx}"',
+			},
 		},
 		// Adds an additional folder inside src when scaffolding.
 		// Default is src.
 		folderName: 'src/block',
 		transformer: (view) => {
-
-			const {
-				slug,title,namespace, textdomain
-			} = view;
+			const { slug, title, namespace, textdomain } = view;
 
 			return {
 				...view,
@@ -62,15 +68,28 @@ module.exports = {
 					name: `${namespace}/${slug}`, // Combine namespace and slug
 					title: `${title}`,
 				},
-			}
+			};
 		},
 		// For older node versions (like node 16)
 		// wpScripts: false,
 		// npmDevDependencies: ['@wordpress/scripts@27.9.0', 'prettier', 'stylelint@15.10.3', 'stylelint-config-standard@34.0.0', 'stylelint-webpack-plugin@4.1.1', 'slash', 'eslint', 'eslint-config-prettier', 'eslint-config-standard', 'eslint-plugin-jsx', 'eslint-plugin-react', 'eslint-plugin-react-hooks', 'eslint-webpack-plugin'],
 		// For newer node versions (above 20)
 		wpScripts: true,
-		npmDevDependencies: ['prettier', 'stylelint', 'stylelint-config-standard', 'stylelint-webpack-plugin', 'slash', 'eslint@^8.0.1', 'eslint-config-prettier', 'eslint-config-standard', 'eslint-plugin-jsx', 'eslint-plugin-react', 'eslint-plugin-react-hooks', 'eslint-webpack-plugin']
-
+		npmDevDependencies: [
+			'prettier',
+			'stylelint',
+			'stylelint-config-standard',
+			'stylelint-webpack-plugin',
+			'slash',
+			'eslint@8.57.1',
+			'eslint-config-prettier',
+			'eslint-config-standard',
+			'eslint-plugin-jsx',
+			'eslint-plugin-react',
+			'eslint-plugin-react-hooks',
+			'eslint-webpack-plugin',
+			'cross-env'
+		],
 	},
 	// Copy the plugin-templates to the root of the project folder where the template is used
 	pluginTemplatesPath: join(__dirname, 'plugin-templates'),
